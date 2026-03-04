b2c1c36fa97545d8d60c474a40b59a14f9811559
Jodene Garvey <46690354+JodeneGarvey@users.noreply.github.com>
2026-03-04 14:15:48 -0500

    remove confidential info

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(cors());
 
 // Database connection with MongoDB
 
-mongoose.connect("mongodb+srv://Jodene:CassieBaby1996@cluster0.vt8rdo6.mongodb.net/reactpetpaw");
+mongoose.connect("mongodb+srv://Jodene:Password@cluster0.vt8rdo6.mongodb.net/reactpetpaw");
 
 //API Creation
 
